037e69da2eebdd51e91c1400481344f129b9ffce
tapatio1 <david159134@hotmail.com>
2021-12-15 16:05:03 -0500

    Prueba practica desarrollador backend y frontend Konecta

---
+++ src/app/components/pages/characters/character-details/character-details.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Params,ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
+import { CharacterService } from '@app/shared/services/character.service';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { Character } from '@app/shared/interface/character.interface';
+@Component({
+  selector: 'app-character-details',
+  templateUrl: './character-details.component.html',
+  styleUrls: ['./character-details.component.scss']
+})
+export class CharacterDetailsComponent implements OnInit {
+  character$!: Observable<Character>;
+
+  
+  constructor(private route:ActivatedRoute, private characterSvc:CharacterService,
+    private location:Location) { }
+
+  ngOnInit(): void {
+    this.route.params.pipe( take(1)).subscribe((params) => {
+      const id = params['id'];
+      this.character$ = this.characterSvc.getDetails(id);
+    });
+  }
+
+  onGoBack(): void{
+    this.location.back();
+    
+  }
+}
